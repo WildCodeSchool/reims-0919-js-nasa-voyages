04643849fdeb0ac358a30a310600469463805e37
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-22 15:07:52 +0200

    Map CSS animations

--- astra/src/components/Map.js
+++ astra/src/components/Map.js
@@ -13,17 +13,17 @@ function PlanetMap() {
 			<div className='orbitSaturn'></div>
 			<div className='orbitUranus'></div>
 			<div className='orbitNeptune'></div>
-			<div className='mercury'></div>
-			<div className='venus'></div>
-			<div className='earth'>
+			<div className='mercury'><img src={"https://zupimages.net/up/19/43/59ta.png"} alt='Sun'/></div>
+			<div className='venus'><img src={"https://zupimages.net/up/19/43/j3cb.png"} alt='Sun'/></div>
+			<div className='earth'><img src={"https://zupimages.net/up/19/43/3sov.png"} alt='Sun'/>
 				<div className='orbitMoon'></div>
-				<div className='moon'></div>
+				<div className='moon'><img src={"https://zupimages.net/up/19/43/7jk4.png"} alt='Sun'/></div>
 			</div>
-			<div className='mars'></div>
-			<div className='jupiter'></div>
-			<div className='saturn'></div>
-			<div className='uranus'></div>
-			<div className='neptune'></div>
+			<div className='mars'><img src={"https://zupimages.net/up/19/43/vh19.png"} alt='Sun'/></div>
+			<div className='jupiter'><img src={"https://zupimages.net/up/19/43/opag.png"} alt='Sun'/></div>
+			<div className='saturn'><img src={"https://zupimages.net/up/19/43/vjxo.png"} alt='Sun'/></div>
+			<div className='uranus'><img src={"https://zupimages.net/up/19/43/926v.png"} alt='Sun'/></div>
+			<div className='neptune'><img src={"https://zupimages.net/up/19/43/stae.png"} alt='Sun'/></div>
 		</div>	
 	);	
 }
