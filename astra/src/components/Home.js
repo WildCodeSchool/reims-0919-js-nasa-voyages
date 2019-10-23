e019ac2a10efdc73da54067f79b4e6f227db6690
Brolyus <desousanthony@gmail.com>
2019-10-23 10:59:27 +0200

    created a homepage in Home.js, added some css(Home.css), and made it appear on App.js

---
+++ astra/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Home.css"
+
+class Home extends React.Component {
+  render () {
+    return (
+      <div>
+        <img src="https://apod.nasa.gov/apod/image/heao_fleet_big.gif" alt="image satellites" />
+      </div>
+    )
+  }
+}
+
+
+export default Home
