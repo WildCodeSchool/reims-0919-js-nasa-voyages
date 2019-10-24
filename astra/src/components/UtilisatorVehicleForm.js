4cba9d847147d382d04d307f1da87124602267cd
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-24 10:30:15 +0200

    Utilisator's vehicle form finished, no css

--- astra/src/components/UtilisatorVehicleForm.js
+++ astra/src/components/UtilisatorVehicleForm.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function UtilisatorVehicleForm () {
-  return(
-    <div>
-      <div>
-        <label for='NameOfVehcile'>Name of your vehicle</label>
-        <input type='text' name='NameOfVehicle' id='NameOfVehicle'></input>
-      </div>
-      <div>
-        <label for="speed">Speed average</label>
-        <input type="number" name="speed" id="speedinput"></input>
-      </div>
-    </div>
+class UtilisatorVehicleForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vehicle: '',
+      speed: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSpeedChange = this.handleSpeedChange.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({ vehicle: event.target.value });
+    console.log(this.state.vehicle)
+  }
+
+  handleSpeedChange(event) {
+    this.setState({ speed: event.target.value });
+    console.log(this.state.speed)
+  }
+  
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='vehicleName'>Vehicle : </label>
+        <input
+          id='vehicleName'
+          name='vehicleName'
+          type='text'
+          value={this.state.vehicle}
+          onChange={this.handleChange}
+        />  
+        <p>{this.state.vehicle}</p>
+      
+        <label htmlFor='vehicleName'>Speed Average : </label>
+        <input
+          id='speedAverage'
+          name='speedAverage'
+          type='number'
+          value={this.state.speed}
+          onChange={this.handleSpeedChange}
+        />  
+        <p>{this.state.speed}</p>
+      </form>
     )
+  }
 }
 
 export default UtilisatorVehicleForm
