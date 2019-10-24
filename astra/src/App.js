425ce207e2f007a63fc127228b7423dee8cbdc0d
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-24 17:17:42 +0200

    Deletion of calling of Navbar in App.js

--- astra/src/App.js
+++ astra/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import UtilisatorVehicleForm from './components/UtilisatorVehicleForm';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> 
       <UtilisatorVehicleForm />
     </div>
   );
