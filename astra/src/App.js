68f8c22d68a4f5e8d2d8c18225173fe40b1d0a16
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-11-06 15:03:06 +0100

    branch dev added + margin 0 and padding 0 on Home.css

--- astra/src/App.js
+++ astra/src/App.js
@@ -4,11 +4,13 @@ import PlanetData from './components/PlanetData'
 import DwarfPlanetData from './components/DwarfPlanetData'
 import SatelliteData from './components/SatelliteData'
 import SunData from './components/SunData'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className = "App">
       <div>
+        <Home />
         <PlanetData />
         <DwarfPlanetData />
         <SatelliteData />
