e2b2126f205c87bba56b99f688748e05b54fbe34
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-11-13 16:54:10 +0100

    Add Manaf's work to mine

--- astra/src/data/vehicleList.js
+++ astra/src/data/vehicleList.js
@@ -1,20 +1,53 @@
 const vehicleList = [
   {
-    vehicle: "Car",
+    vehicle: "Chameau",
+    speed: "65"
+  },
+  {
+    vehicle: "Moto",
     speed: "120"
   },
   {
-    vehicle: "Rocket",
-    speed: "39895"
+    vehicle: "Fusée",
+    speed: "12000"
   },
   {
-    vehicle: "Bicyle",
+    vehicle: "Vélo",
     speed: "20"
   },
   {
-    vehicle: "Camel",
-    speed: "65"
+    vehicle: "Avion de chasse",
+    speed: "2778"
+  },
+  {
+    vehicle: "Airbus A380",
+    speed: "1185"
+  },
+  {
+    vehicle: "Hélicoptère de combat",
+    speed: "357"
+  },
+  {
+    vehicle: "Tank",
+    speed: "90"
+  },
+  {
+    vehicle: "Port-avion",
+    speed: "56"
+  },
+  {
+    vehicle: "TGV",
+    speed: "574"
+  },
+  {
+    vehicle: "Course à pied",
+    speed: "10"
+  },
+  {
+    vehicle: "Bugatti Veyron",
+    speed: "431"
   }
+
 ];
 
 export default vehicleList
